6b33e0a35d8ff1a92be872b3b46e6bd43b07bf6b
Bianca Vargas <biancavn06@gmail.com>
2023-10-26 15:02:52 -0300

    mario

--- mario/mario.js
+++ mario/mario.js
@@ -1,15 +1,39 @@
 const mario = document.querySelector('.mario');
+const cano = document.querySelector('.cano');
 
 
 const jump = () => {
 mario.classList.add('jump');
 
 setTimeout(() => {
-
     mario.classList.remove('jump');
-
-
 }, 500);
 }
+
+const loop = setInterval(()=>{
+
+    console.log('loop')
+
+    const canoPosition = cano.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+    console.log(marioPosition);
+
+    if (canoPosition <= 120 && canoPosition > 0 && marioPosition < 80){
+
+        cano.style.animation = 'none';
+        cano.style.left = `${canoPosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.left = `${marioPosition}px`;
+
+        mario.src = './images/morto.png';
+        mario.style.width = '75px'
+        mario.style.marginLeft = '50px'
+
+        clearInterval(loop);
+    }
+
+},10)
 
 document. addEventListener('keydown', jump)
